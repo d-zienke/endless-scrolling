6c18d6b79c337311afac866ac03f03ebdead5e9f
Damian Zienke <zienke.damian@gmail.com>
2023-05-18 17:19:22 +0200

    Split code into modules
    * new folder for functions has been added (`functions`)
    * functions for fetching and rendering images have been moved to new separate file (`images-api.js`)

--- app.js
+++ app.js
@@ -1,68 +1,9 @@
-// select DOM elements
-const imgListContainer = document.getElementById("images-list");
+import { getImages } from "./functions/images-api.js";
 
-// specify base URL
-const baseUrl = `https://picsum.photos/1920/1080?random=1.webp`;
+// select DOM elements
+export const imgListContainer = document.getElementById("images-list");
 
 const imagesPerFetch = 5;
 
 // initial images
 getImages(imagesPerFetch);
-
-// get and render images
-function getImages(amount) {
-	try {
-		for (let i = 0; i < amount; i++) {
-			getFinalUrl(baseUrl);
-		}
-	} catch (error) {
-		throw new Error(error);
-	}
-}
-
-// create and render DOM element for image
-function renderImage(author, srcUrl, imgUrl) {
-	const element = document.createElement("div");
-	element.classList.add("images-list__item");
-	const sourceLink = document.createElement("a");
-	sourceLink.setAttribute("href", srcUrl);
-	sourceLink.setAttribute("target", "_blank");
-	const image = document.createElement("img");
-	image.classList.add("images-list__image");
-	image.setAttribute("src", imgUrl);
-	image.setAttribute("title", `Image by: ${author}`);
-	sourceLink.appendChild(image);
-	element.appendChild(sourceLink);
-	imgListContainer.appendChild(element);
-}
-
-// use final URL to get image's metadata
-async function getImgMetadata(url) {
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		renderImage(data.author, data.url, data.download_url);
-	} catch (error) {
-		throw new Error(error);
-	}
-}
-
-// retrieve final destination URL
-async function getFinalUrl(baseUrl) {
-	try {
-		const response = await fetch(baseUrl);
-		const finalUrl = response.url;
-		// find the starting index of ID substring
-		const imgIdIndexStart = finalUrl.indexOf("id/") + 3;
-		// get ID substring
-		const imgIdStart = finalUrl.slice(imgIdIndexStart);
-		// check how many letters has ID
-		const imgIdIndexLength = imgIdStart.indexOf("/");
-		// get image's ID
-		const imgId = imgIdStart.slice(0, imgIdIndexLength);
-		const imgUrl = `https://picsum.photos/id/${imgId}/info`;
-		getImgMetadata(imgUrl);
-	} catch (error) {
-		throw new Error(error);
-	}
-}
